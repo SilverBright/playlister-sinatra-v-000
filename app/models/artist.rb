78f65f4c4d7f3ff1504b2ec7eb4b8865f7bdd01f
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-24 23:51:55 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,12 @@
 class Artist < ActiveRecord::Base
   attr_accessor :name, :song, :genre
 
-  def initialize(name, song, genre)
-    @name = name
-    @song = song
-    @genre = genre
+  def initialize(params)
+    @name = params[:name]
+    @song = params[:song]
+    @genre = params[:genre]
   end
+
 
   has_many :songs
   has_many :genres
