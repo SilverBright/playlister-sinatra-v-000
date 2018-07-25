8304dfa6bfe1e038ad54c147c5eae6b78e54cd2e
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-25 02:18:45 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,12 @@ class SongsController < ApplicationController
   get '/songs/:slug' do
       @song = Song.find_by_slug(params[:slug])
       erb :'/songs/show'
-    end
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
+
 
 end
