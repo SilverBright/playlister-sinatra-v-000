33615b40c8e0c26ac905c56265dc03ef647ecb64
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-25 00:59:53 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,4 +7,9 @@ class Artist < ActiveRecord::Base
     name.gsub(" ", "-").downcase
   end
 
+  def self.find_by_slug(slug)
+    Artist.all.find do |artist|
+      artist.slug == slug
+    end
+
 end
