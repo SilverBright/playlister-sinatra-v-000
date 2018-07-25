589891f15dc8870af129eb5084b400d163cce2f0
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-25 02:27:45 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,20 @@
+require 'rack-flash'
 class SongsController < ApplicationController
+
+ use Rack::Flash
 
   get '/songs' do
     @songs = Song.all
     erb :'/songs/index'
   end
 
-  get '/songs/:slug' do
-      @song = Song.find_by_slug(params[:slug])
-      erb :'/songs/show'
-  end
-
   get '/songs/new' do
     erb :'/songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
   end
 
   post '/songs' do
@@ -20,10 +23,26 @@ class SongsController < ApplicationController
     @song.genre_ids = params['genres']
     @song.save
 
+    flash[:message] = "Successfully created song."
+
     redirect "/songs/#{@song.slug}"
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
 
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    binding.pry
+    @song.save
 
+    flash[:message] = "Successfully updated song."
+
+    redirect "/songs/#{@song.slug}"
+  end
 
 end
