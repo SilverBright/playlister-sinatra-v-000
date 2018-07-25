14eec1393289a1406e1dc2ad0c54a3c49afd2d0f
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-25 01:16:47 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,4 +4,15 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: 'song_genres'
 
+  def slug
+    name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    Song.all.find do |song|
+      song.slug == slug
+    end
+  end
+
+
 end
